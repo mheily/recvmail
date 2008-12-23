1a36c693a6a39e753fe2de767e8762b3487a5431
mheily <mheily@f7bf37ba-733a-0410-8912-73af84f046af>
2008-12-23 02:01:07 +0000

    more debugging

--- server.c
+++ server.c
@@ -322,16 +322,17 @@ server_dispatch(struct server *srv)
 
         for (i = 1; i < srv->pfd_count; i++) {
             if (srv->pfd[i].revents & (POLLERR|POLLHUP|POLLNVAL)) {
-                log_debug("POLLERR/HUP on session %d", i);
                 if ((s = session_lookup(srv, srv->pfd[i].fd)) == NULL)
                     errx(1, "session_lookup failed");
+                log_debug("POLLERR/HUP/NVAL on session %d (fd %d)", i, s->fd);
                 session_close(s);
                 free(s); //XXX-FIXME-recycle it.        
             } else if (srv->pfd[i].revents & POLLIN) {
-                log_debug("POLLIN on session %d", i);
 
                 if ((s = session_lookup(srv, srv->pfd[i].fd)) == NULL)
                     errx(1, "session_lookup failed");
+
+                log_debug("POLLIN on session %d (fd %d)", i, s->fd);
                 
                 session_read(s);
              
