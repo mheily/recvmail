1a6be1646f6adc46920f07ea4e80e02f7c907110
mheily <mheily@f7bf37ba-733a-0410-8912-73af84f046af>
2009-05-03 18:53:10 +0000

    doc fixes for 2.0

--- trunk/www/download.php
+++ trunk/www/download.php
@@ -2,8 +2,14 @@
 
 <h2>Download</h2>
 
+<h3>Source Releases</h3>
+
 Periodic <a href="http://code.google.com/p/recvmail/downloads/list">source code releases</a> are made available as tarballs. There are no binary packages available at this time.
 <p>
+The current release is 
+<a href="http://recvmail.googlecode.com/files/recvmail-<?php echo "$stable_version"?>.tar.gz">recvmail-<?php echo "$stable_version"?>.tar.gz</a>.
+
+<h2>Browse</h2>
 The <a href="http://recvmail.googlecode.com/svn/trunk/">SVN repository</a> can be viewed in your web browser, or you can use this command to anonymously check out the latest project source code:
 <pre>
 svn checkout http://recvmail.googlecode.com/svn/trunk/ recvmail
