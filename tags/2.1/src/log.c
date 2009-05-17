ac11696dc4c6e0df992d7ab8e5366cc455e2bd63
mheily <mheily@f7bf37ba-733a-0410-8912-73af84f046af>
2009-05-17 04:21:30 +0000

    2.1

---
+++ tags/2.1/src/log.c
@@ -0,0 +1,47 @@
+/*		$Id$		*/
+
+/*
+ * Copyright (c) 2009 Mark Heily <devel@heily.com>
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+#include "log.h"
+
+static char *_ident;
+
+int detached = 0;
+int log_level = LOG_INFO;
+int log_is_open = 0;
+
+void
+log_open(const char *ident, int option, int facility, int level)
+{
+    /* Make a permanent copy of the "ident" string */
+    if ((_ident = strdup(ident)) == NULL)
+        abort();
+
+    openlog(_ident, LOG_NDELAY | LOG_PID | option, facility);
+    setlogmask(LOG_UPTO(level));
+    
+    log_level = level;
+    log_is_open = 1;
+}
+
+void
+log_close(void)
+{
+    closelog();
+    free(_ident);
+    _ident = NULL;
+}
---
+++ tags/2.1/src/options.h
@@ -0,0 +1,26 @@
+#ifndef _OPTIONS_H
+#define _OPTIONS_H
+
+#include <stdbool.h>
+
+/* Configuration options */
+
+#define CHROOTDIR            "/srv/mail"
+
+struct options {
+    u_short     port;
+    bool        debugging;
+    char       *hostname;
+    bool        daemon;	        /* Run as daemon ? */
+    char       *log_ident;	    /* Program name to use in syslog */
+    int         log_facility;	/* The log facility to provide to syslog(3) */
+    int         log_level;	    /* The level used by setlogmask(3) */
+    char       *uid;            /* The user ID to run under */
+};
+
+extern struct options OPT;
+
+//TODO: move from server.c to options.c
+int options_parse(int argc, char *argv[]);
+
+#endif  /* _OPTIONS_H */
