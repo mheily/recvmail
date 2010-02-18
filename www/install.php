fcc4361502090ad74956da27cf91fc7abf2a711b
mheily <mheily@f7bf37ba-733a-0410-8912-73af84f046af>
2010-02-18 03:03:10 +0000

    docfix

--- www/install.php
+++ www/install.php
@@ -4,13 +4,15 @@
 
 <h3>FreeBSD</h3>
 <ol>
-<li>./configure && make && sudo make install</li>
-<li>su</li>
-<li># sudo pw group add recvmail</li>
-<li># sudo pw useradd -c 'recvmail daemon' -d /srv/mail -g recvmail -L daemon -s /bin/false -m -n recvmail</li>
-<li># chmod 700 /srv/mail</li>
-<li># sudo -u recvmail mkdir -p /srv/mail/spool/{new,cur,tmp}</li>
-<li># sudo -u recvmail mkdir -p /srv/mail/box/recvmail.org</li>
+<li>$ ./configure && make</li>
+<li>$ su</li>
+<li># make install</li>
+<li># pw group add recvmail</li>
+<li># pw useradd -c 'recvmail daemon' -d /srv/mail -g recvmail -L daemon -s /bin/false -n recvmail</li>
+<li># mkdir -p /srv/mail/spool/{new,cur,tmp}</li>
+<li># mkdir -p /srv/mail/box/recvmail.org</li>
+<li># chmod -R 700 /srv/mail</li>
+<li># chown -R recvmail:recvmail /srv/mail</li>
 </ol>
 
 <?php include "footer.html"?>
